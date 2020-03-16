7384bd3ceff81880f2eedd706ec08c28d3dcb7e2
Hassan Murtaza <mrhassanmurtaza@gmail.com>
2020-03-16 11:55:08 +0500

    :100: Initialize Repo

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # ec2-ssh
 Wrapper around AWS Session Manager to establish ssh sessions using public ip, private ip and instance id
+
+## Usage
+```bash
+
+   SSH to instances using SSM Session Manager.
+   usage: ec2-ssh [-ip | --public-ip || -pip | --private-ip || -id | --instance-id] [-r | --region] [-p | --profile]
+   
+   To see help text, you can run   
+   
+   ec2-ssh [-h | --help]  Help for running this script.
+   
+   required flags:
+   -ip || -pip || id       IP Address, Private IP or Instance ID of the instance to do SSH
+   -r (default: us-east-1) Region in which instance resides
+   -p (default: None)      Profile for aws ssm session manager
+   
+   ec2-ssh: error: the following arguments are required: [-ip | --public-ip || -pip | --private-ip || -id | --instance-id] [-r | --region] [-p | --profile]
+```
+
+## Example Usage
+
+* Using Public IP:
+`ec2-ssh --profile=<your_aws_profile> --region=<ec2_aws_region> --public-ip=<public_ip_for_instance>`
+
+* Using Private IP:
+`ec2-ssh --profile=<your_aws_profile> --region=<ec2_aws_region> --private-ip=<private_ip_for_instance>`
+
+* Using Instance ID:
+`ec2-ssh --profile=<your_aws_profile> --region=<ec2_aws_region> --instance-id=<instance_id_for_instance>`
+
+* Short using Public IP:
+`ec2-ssh -p=<your_aws_profile> -r=<ec2_aws_region> -ip=<public_ip_for_instance>`
+
+* Short using Private IP:
+`ec2-ssh -p=<your_aws_profile> -r=<ec2_aws_region> -pip=<private_ip_for_instance>`
+
+* Short using Instance ID:
+`ec2-ssh -p=<your_aws_profile> -r=<ec2_aws_region> -id=<instance_id_for_instance>`
+
+## Prerequisites
+
+* AWS CLI version 1.16.299 or higher
+* AWS session manager plugin ([see the install documentation](https://docs.aws.amazon.com/systems-manager/latest/userguide/session-manager-working-with-install-plugin.html))
---
+++ ec2-ssh.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+
+# Wrapper around AWS session manager for instance access using public ip and private ip
+scriptname=$0
+
+# Defaults
+region='us-east-1'
+profile=''
+
+################################################################################
+# Help                                                                         #
+################################################################################
+Help()
+{
+   # Display Help
+   echo "SSH to instances using SSM Session Manager."
+   echo "usage: $scriptname [-ip | --public-ip || -pip | --private-ip || -id | --instance-id] [-r | --region] [-p | --profile]"
+   echo
+   echo "To see help text, you can run:"
+   echo 
+   echo "$scriptname [-h | --help]  Help for running this script."
+   echo ""
+   echo "required flags:"
+   echo "-ip || -pip || id       IP Address, Private IP or Instance ID of the instance to do SSH"
+   echo "-r (default: us-east-1) Region in which instance resides"
+   echo "-p (default: None)      Profile for aws ssm session manager"
+   echo ""
+   echo "$scriptname: error: the following arguments are required: [-ip | --public-ip || -pip | --private-ip || -id | --instance-id] [-r | --region] [-p | --profile]"
+}
+
+compareable_version() {
+  echo "$@" | awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4); }';
+}
+
+# Make sure there is a version of the AWS CLI that supports session manager. Thanks to: https://github.com/rewindio/aws-connect/
+minimum_aws_cli_version=1.16.299
+current_aws_cli_version=$(aws --version 2>&1 | awk '{split($1,array,"/")} END {print array[2]}')
+
+if [ "$(compareable_version "${current_aws_cli_version}")" -lt "$(compareable_version "${minimum_aws_cli_version}")" ]; then
+  echo "Error: AWS CLI version must be greater than ${minimum_aws_cli_version}. Please update your aws cli (pip install awscli --upgrade)"
+  exit 1
+fi
+
+# checking if session manager plugin exists
+if [ ! -e /usr/local/bin/session-manager-plugin ]; then
+  echo "AWS SessionManagerPlugin is not found - installing"
+  echo "See the AWS Session Manager Plugin Docs for more information: http://docs.aws.amazon.com/console/systems-manager/session-manager-plugin-not-found"
+  exit 1
+fi
+
+
+if [ $# -eq 0 ] || [ $1 == "-h" ] || [ $1 == "--help" ];
+    then
+        Help
+        exit 1
+fi
+
+# looking through variables
+for arg in "$@"
+do
+
+index=$(echo $arg | cut -f1 -d=)
+val=$(echo $arg | cut -f2 -d=)
+
+case $index in
+    -ip | --public-ip | -pip | --private-ip | -id | --instance-id) 
+                operation=$index
+                opvalue=$val
+            ;;
+    -r | --region) 
+                region=$val
+                ;;
+    -p | --profile)
+                profile=$val
+                ;;
+    *) 
+                echo "Incorrect options provided"
+                Help 
+                exit 1 
+    ;;
+esac
+done
+
+if [ $operation == "-ip" ] || [ $operation == "--public-ip" ];
+then
+    if [[ $opvalue =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+        target=$(aws ec2 describe-instances --filter Name=ip-address,Values="${opvalue}" --query Reservations[].Instances[].InstanceId --output text --region "${region}" --profile "${profile}")
+    else
+        echo "Please enter a valid public ip address syntax. e.g. 55.54.53.52"
+        exit 1
+    fi
+
+elif [ $operation == "-pip" ] || [ $operation == "--private-ip" ];
+then
+    if [[ $opvalue =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+        target=$(aws ec2 describe-instances --filter Name=private-ip-address,Values="${opvalue}" --query Reservations[].Instances[].InstanceId --output text --region "${region}" --profile "${profile}")
+    else
+        echo "Please enter a valid private ip address syntax. e.g. 192.168.0.1"
+        exit 1
+    fi
+elif [ $operation == "-id" ] || [ $operation == "--instance-id" ];
+then
+        target=$opvalue
+fi
+
+# # AWS SSM Session Manager Start Session
+aws ssm start-session \
+    --target "${target}" \
+    --region "${region}" \
+    --profile "${profile}"
